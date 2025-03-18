ddad550334a61ea0fc1f7d0479fdfb9f03f14d68
Paul Tondeur <paul@paultondeur.com>
2025-03-18 14:31:36 +0100

    feat: adds tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: 'Subrise | WBSO-aanvragen eenvoudiger dan ooit',
@@ -24,6 +25,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="nl">
+      <head>
+        <Script
+          defer
+          data-domain="subrise.eu"
+          src="https://pa.paultondeur.com/js/script.hash.outbound-links.tagged-events.js"
+          strategy="afterInteractive"
+        />
+        <Script id="plausible-setup" strategy="afterInteractive">
+          {`window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }`}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   )
