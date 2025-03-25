4e47fd85799777a7746ae3c55d0a7c89d2521b08
Paul Tondeur <paul@paultondeur.com>
2025-03-25 14:53:10 +0100

    fix: permanent redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,9 @@
 import { NextResponse } from 'next/server';
 
 export function middleware() {
-  return NextResponse.redirect('https://wbso.ai');
+  return NextResponse.redirect('https://wbso.ai', {
+    status: 308 // Permanent redirect
+  });
 }
 
 export const config = {
